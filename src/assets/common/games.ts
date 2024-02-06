1b8b19fda159da2df2efb14b14ead84e97f7e147
Jasper Stephenson <jasperstephenson@gmail.com>
2024-02-06 20:11:12 +0900

    prelive

--- src/assets/common/games.ts
+++ src/assets/common/games.ts
@@ -117,7 +117,7 @@ export const games: Game[] = [
       'アーティスト・William Zack Woodによる、即興演劇やダンス・ムーブメントの実践から学んだシンプルなゲーム。ゲーム終了後は、体験を振り返るために小冊子を作ります。',
     venue: venues.bridgestudio,
     designer: designers.zack,
-    ticketUrl: 'https://peatix.com/group/15427896/events',
+    ticketUrl: 'coming soon',
   },
   {
     image: '/images/games/jam.jpg',
@@ -131,7 +131,7 @@ export const games: Game[] = [
       'Playtest運営チームによる、1日で新しいゲームのプロトタイプを作るGame Jamを開催。街なかや公共空間を舞台にしたフィジカルなゲームのアイデアをチームで出し、プロトタイプを作り、検証・発表までを行うワークショップです。',
     venue: venues.bridgestudio,
     designer: designers.playtest,
-    ticketUrl: 'https://peatix.com/group/15427896/events',
+    ticketUrl: 'https://peatix.com/event/3843397/view',
   },
   {
     image: '/images/games/joanna.webp',
@@ -141,7 +141,7 @@ export const games: Game[] = [
       '街角のあらゆるものを普通とは違う方法でつなげる、共同作業と競争ゲーム。各チームは60秒以内に、周囲の建物、構造物、物、人などのあらゆるものを使い、できるだけ多くの辺を持つ閉じた形を1つ作ります。',
     venue: venues.various,
     designer: designers.joanna,
-    // ticketUrl: 'https://peatix.com/group/15427896/events',
+    ticketUrl: 'coming soon',
   },
   {
     image: '/images/games/kaho.png',
@@ -151,7 +151,7 @@ export const games: Game[] = [
       '誰かと手をつないだり、触れ合ったりすることで音の変化を生み出す、遊び心のあるインタラクティブなインスタレーションを、常設展示会場にて展示します。',
     venue: venues.bridgestudio,
     designer: designers.kaho,
-    // ticketUrl: 'https://peatix.com/group/15427896/events',
+    ticketUrl: 'coming soon',
   },
   {
     image: '/images/games/marcus.jpg',
@@ -159,9 +159,9 @@ export const games: Game[] = [
     description: `Make your own Hanafuda cards with a stencilling technique called kappazuri! `,
     descriptionJa:
       '花札と言えば、今でこそ機械刷りのものが当たり前ですが、江戸時代から昭和初期にかけて京都では「合羽摺り」というステンシルを使った手摺りが一般的でした。戦後に姿を消したこの印刷方法は昭和50年代に一度復活されたものの、手摺りの花札を手に入れようと思えば、今日ではとても高価です。この手摺り花札を一緒に作り、みんなで遊んでみましょう。',
-    venue: venues.hohohoza,
+    venue: venues.bridgestudio,
     designer: designers.marcus,
-    ticketUrl: 'https://peatix.com/group/15427896/events',
+    ticketUrl: 'coming soon',
   },
   {
     image: '/images/games/tomo.png',
@@ -171,6 +171,6 @@ export const games: Game[] = [
       'デジタル化が進んだ今、純粋に道に迷うことは珍しい。この都市型ゲームは、迷子になったときの興奮を追体験させてくれるものです。プレイヤーは目隠しをし、連れて行かれた場所で、自分の感覚だけを頼りに、手がかりを解き明かし、決められた場所への道を見つけなければなりません。この冒険では、テクノロジーへの依存を掘り下げ、都市探検と周囲とのつながりを取り戻すという新鮮な視点を提示します。',
     venue: venues.cityhall,
     designer: designers.tomo,
-    ticketUrl: 'https://peatix.com/group/15427896/events',
+    ticketUrl: 'coming soon',
   },
 ]
--- src/components/Home/Calendar.vue
+++ src/components/Home/Calendar.vue
@@ -15,9 +15,7 @@ import * as appState from '~/assets/state'
 const i18n = useI18n()
 const locale = computed(() => i18n.locale.value)
 
-const calendarText = `3/29 (Fri) | Jam [10:00-15:00], X [12:00-19:00]
-3/30 (Sat) | Jam [8:00-15:00], X [17:00-19:00]
-3/31 (Sun) | Jam [10:00-15:00], X [14:00-19:00]`
+const calendarText = `3/29 (Fri) | Physical Game Jam [13:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00]
+3/30 (Sat) | PLAY SESSION [13:00-15:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00]
+3/31 (Sun) | Unmapping [12:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], DIY Hanafuda Workshop [13:00-16:00]`
 </script>
-
-<style lang="scss" scoped></style>
