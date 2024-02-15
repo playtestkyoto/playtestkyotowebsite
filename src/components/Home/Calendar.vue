32a9a688fcd9e346968ec00ffae4bb4d70cf18e4
Jasper Stephenson <jasperstephenson@gmail.com>
2024-02-15 17:30:12 +0900

    cal

--- src/components/Home/Calendar.vue
+++ src/components/Home/Calendar.vue
@@ -17,5 +17,5 @@ const locale = computed(() => i18n.locale.value)
 
 const calendarText = `3/29 (Fri) | Physical Game Jam [13:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00]
 3/30 (Sat) | PLAY SESSION [13:00-15:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00]
-3/31 (Sun) | Unmapping [12:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], DIY Hanafuda Workshop [13:00-16:00]`
+3/31 (Sun) | Unmapping (Group 1) [13:00-15:00], Unmapping (Group 2) [16:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], DIY Hanafuda Workshop [13:00-16:00]`
 </script>
