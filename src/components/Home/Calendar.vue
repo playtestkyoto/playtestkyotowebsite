85a6669577985183339717df86dc848a9aef7205
Jasper Stephenson <jasperstephenson@gmail.com>
2024-03-05 14:59:47 +0900

    play

--- src/components/Home/Calendar.vue
+++ src/components/Home/Calendar.vue
@@ -16,6 +16,6 @@ const i18n = useI18n()
 const locale = computed(() => i18n.locale.value)
 
 const calendarText = `3/29 (Fri) | Physical Game Jam [13:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], Urban Lens [12:00-18:00]
-3/30 (Sat) | PLAY SESSION [13:00-15:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], Urban Lens [12:00-18:00]
+3/30 (Sat) | PLAY SESSION (Group 1) [13:00-15:00], PLAY SESSION (Group 2) [16:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], Urban Lens [12:00-18:00]
 3/31 (Sun) | Unmapping (Group 1) [13:00-15:00], Unmapping (Group 2) [16:00-18:00], Warp and Weft [12:00-13:00], Touch [12:00-18:00], DIY Hanafuda Workshop [13:00-16:00], Urban Lens [12:00-18:00]`
 </script>
