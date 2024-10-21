0f88449ee67b6f0e1d02b53212d064293ef5e5eb
Jasper Stephenson <jasperstephenson@gmail.com>
2024-10-22 07:43:19 +0900

    video

---
+++ src/components/Home/Video.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="videoEmbed contentcolumn martop flexcenter">
+    <iframe
+      width="100%"
+      height="auto"
+      src="https://www.youtube.com/embed/mF9YKfflbKE?si=yyIe7HOGmLR5wMVA"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as c from '~/assets/common'
+import * as appState from '~/assets/state'
+const i18n = useI18n()
+const locale = computed(() => i18n.locale.value)
+</script>
+
+<style lang="scss" scoped>
+.videoEmbed {
+  iframe {
+    aspect-ratio: 16 / 9;
+  }
+  @media (max-width: 768px) {
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,8 @@
 
     <HomePhotoLink />
 
+    <HomeVideo />
+
     <div class="marbotbig">
       <HomeAbout />
     </div>
