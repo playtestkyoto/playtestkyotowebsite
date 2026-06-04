c6f8a5def8209607427b951266acbb380f9ea685
Jasper Stephenson <jasperstephenson@gmail.com>
2026-06-04 14:14:18 +0900

    latest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
   // devtools: { enabled: true },
   srcDir: 'src/',
   devServer: {
-    port: 3030,
+    port: 3031,
   },
-  typescript: {
-    typeCheck: true,
-  },
+  // typescript: {
+  //   typeCheck: true,
+  // },
   css: ['~/assets/css/main.scss'],
   app: {
     head: {
@@ -19,30 +19,25 @@ export default defineNuxtConfig({
           content: 'width=device-width, initial-scale=1',
         },
         {
-          hid: 'description',
           name: 'description',
           content:
             '都市空間を舞台としたゲームや遊びを行う3日間のイベント。 A three-day event for games and play in urban spaces.',
         },
         {
-          hid: 'og:title',
           property: 'og:title',
           content: 'Playtest Kyoto 2024',
         },
         {
-          hid: 'og:description',
           property: 'og:description',
           content:
             '都市空間を舞台としたゲームや遊びを行う3日間のイベント。 A three-day event for games and play in urban spaces.',
         },
         {
-          hid: 'og:image',
           property: 'og:image',
           content:
             'https://playtest.city/playtest_social.jpg',
         },
         {
-          hid: 'og:url',
           property: 'og:url',
           content: 'https://playtest.city/',
         },
